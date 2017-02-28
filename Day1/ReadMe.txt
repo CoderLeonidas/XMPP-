99d4c04ad54bc8c1f8cf2d51a1d39b6cd25ba683
CoderLeonidas <luoyang@luoyangdeMacBook-Pro.local>
2017-02-28 19:09:37 +0800

    initial commit

---
+++ Day1/ReadMe.txt
@@ -0,0 +1,4 @@
+注意：
+chatserver.py 为一个建议的聊天室服务器脚本，直接在终端 python chatserver.py即可运行。
+
+若py脚本运行报错：ImportError: No module named twisted.internet，可以看我的这篇文章：http://www.jianshu.com/p/7d7e255f008c
---
+++ Day1/chatserver.py
@@ -0,0 +1,41 @@
+from twisted.internet.protocol import Protocol, Factory
+from twisted.internet import reactor
+
+class IphoneChat(Protocol):
+	"""docstring for IphoneChat"""
+	def connectionMade(self):
+		#self.transport.write("""connected""")
+		self.factory.clients.append(self)
+		print "clients are ", self.factory.clients
+
+	def connectionLost(self, reason):
+		self.factory.clients.remove(self)
+	def dataReceived(self, data):
+		#print "data is ", date
+		a = data.split(':')
+		if len(a) > 1:
+			command = a[0]
+			content = a[1]
+
+			msg = ""
+			if  command == "iam":
+				self.name = content 
+				msg = self.name + ": " + content
+			elif command == "msg":
+				msg = self.name + ": " +content
+
+			print msg
+
+			for c in self.factory.clients:
+				c.message(msg)
+
+	def  message(self, message):	
+		self.transport.write(message + '\n')
+
+factory = Factory()
+factory.protocol = IphoneChat
+factory.clients = []
+
+reactor.listenTCP(12345, factory)
+print "Iphone Chat server started"
+reactor.run()
